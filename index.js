b7633563078133eb58a348ca09ca7dec376306bb
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-06-29 20:22:51 +0430

    fix: MODULE_NOT_FOUND Error in indexjs

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ const app = express();
 
 app.use(express.json());
 
-const CLIENT_DIR = process.cwd();
-let CONFIG = {};
 try {
-  CONFIG = require(`${CLIENT_DIR}\\jm-api.config.js`);
-} catch (error) {}
+  const CLIENT_DIR = process.cwd();
+  const CONFIG = require(`${CLIENT_DIR}\\jm-api.config.js`);
 
-app.get('/api', (req, res) => {
-  res.send('Hello World');
-});
+  app.get('/api', (req, res) => {
+    res.send('Hello World');
+  });
 
-const PORT = CONFIG.port || 5000;
-app.listen(PORT, () =>
-  console.log(`Server is listening: http://localhost:${PORT}/api/`)
-);
+  const PORT = CONFIG.port || 5000;
+  app.listen(PORT, () =>
+    console.log(`Server is listening: http://localhost:${PORT}/api/`)
+  );
+} catch (error) {
+  if (error.code === 'MODULE_NOT_FOUND') {
+    console.log('The "jm-api.config.js" file does not exist!');
+  } else {
+    console.log(error);
+  }
+}
