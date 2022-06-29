c8050b4766277a4b877a033f1aa28417dc136612
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-06-29 20:06:27 +0430

    feat: Support user's custom configuration

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const app = express();
 
 app.use(express.json());
 
+const CLIENT_DIR = process.cwd();
+let CONFIG = {};
+try {
+  CONFIG = require(`${CLIENT_DIR}\\jm-api.config.js`);
+} catch (error) {}
+
 app.get('/api', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(5000, () =>
-  console.log('Server is listening http://localhost:5000/api/...')
+const PORT = CONFIG.port || 5000;
+app.listen(PORT, () =>
+  console.log(`Server is listening: http://localhost:${PORT}/api/`)
 );
