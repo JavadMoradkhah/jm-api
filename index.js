83c42165a5d2cf749d4567a7b80885718d77b7e1
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-06-29 19:08:35 +0430

    feat: Setup Express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(5000, () =>
+  console.log('Server is listening http://localhost:5000/api/...')
+);
