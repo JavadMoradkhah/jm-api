7fde034a45f833dcc5755438fcf51f5ec7038718
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-07-06 21:55:50 +0430

    feat: Adding schema for email and password in ConfigSchema.js

--- schema/ConfigSchema.js
+++ schema/ConfigSchema.js
@@ -4,6 +4,14 @@ const { fieldSchema, collectionSchema } = require('./CollectionSchema');
 const Schema = Joi.object({
   port: Joi.number().integer().min(3000).max(9999).default(5000),
   userModel: Joi.object({
+    email: Joi.object({
+      min: Joi.number().integer(),
+      max: Joi.number().integer().max(255),
+    }),
+    password: Joi.object({
+      min: Joi.number().integer(),
+      max: Joi.number().integer().max(255),
+    }),
     fields: Joi.array().min(1).items(fieldSchema).required(),
   }),
   collections: Joi.array().items(collectionSchema).required(),
