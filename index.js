8727980652597f51b1543c6b67ecf6eb8ed93a9c
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-07-06 19:04:26 +0430

    feat: Validating configuration settings

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+const configSchema = require('./schema/ConfigSchema');
 const uploadRoutes = require('./routes/UploadRoutes');
 const generateRoutes = require('./routes/routes');
 const express = require('express');
@@ -12,6 +12,14 @@ try {
   const CLIENT_DIR = process.cwd();
   const CONFIG = require(`${CLIENT_DIR}\\jm-api.js`);
 
+  const configValidationResult = configSchema.validate(CONFIG);
+  if (configValidationResult.error) {
+    console.group('The configuration settings are not set correctly!');
+    console.log(configValidationResult.error.message);
+    console.groupEnd();
+    process.exit(1);
+  }
+
   app.use('/uploads', express.static(`${CLIENT_DIR}\\uploads`));
 
   app.use('/api', uploadRoutes);
@@ -22,10 +30,8 @@ try {
   });
 
   const PORT = CONFIG.port || 5000;
-  app.listen(PORT, () =>
-    console.log(`Server is listening: http://localhost:${PORT}/api/`)
-  );
-} catch (error) {66a
+  app.listen(PORT, () => console.log(`Server is listening: http://localhost:${PORT}/api/`));
+} catch (error) {
   if (error.code === 'MODULE_NOT_FOUND') {
     console.log('The "jm-api.js" file does not exist!');
   } else {
