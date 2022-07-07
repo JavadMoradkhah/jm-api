3cf45945173e69ac76f837b5b4511568606dbbe4
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-07-07 20:22:55 +0430

    feat: Applying defaults.js values as default values

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const defaults = require('../config/defaults');
 const uploadRoutes = require('../routes/UploadRoutes');
 const generateAuthRoutes = require('../routes/AuthRoutes');
 const generateRoutes = require('../routes/routes');
@@ -11,12 +12,14 @@ function startServer(CLIENT_DIR, CONFIG) {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
+  const uploadPath = CONFIG.upload.path || defaults.upload.path;
+
   app.use('/api', uploadRoutes);
   app.use('/api', generateAuthRoutes(CONFIG.userModel, CONFIG.jwtKey));
   app.use('/api', generateRoutes(CONFIG.collections));
-  app.use('/uploads', express.static(path.resolve(CLIENT_DIR, 'uploads')));
+  app.use('/uploads', express.static(path.resolve(CLIENT_DIR, uploadPath)));
 
-  const PORT = CONFIG.port || 5000;
+  const PORT = CONFIG.port || defaults.port;
   const serverSpinner = createSpinner('Starting server...').start();
   app.listen(PORT, () => {
     serverSpinner.success();
